ce32082c6de5d6952fde01b5bdc1907917d2cc9a
Dillion Megida <dillionmegida@gmail.com>
2019-09-06 02:55:22 +0100

    Register and Login Page

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import App from './App';
 import Register from './views/Register';
+import Login from './views/Login';
 import './styles/index.css';
 
 const Routing = (
@@ -10,6 +11,7 @@ const Routing = (
     <Switch>
       <Route exact path='/' component={App} />
       <Route path='/register' component={Register} />
+      <Route path='/login' component={Login} />
     </Switch>
   </Router>
 )

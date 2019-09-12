efe0da99af352849acff8971eab6ee9fba0c38a8
Fayvor George <phavorsparks@gmail.com>
2019-09-13 00:58:08 +0100

    Fix routing issues

--- client/src/User/components/Common/Dp.js
+++ client/src/User/components/Common/Dp.js
@@ -3,28 +3,28 @@ import Pic from '../../assets/deee.png';
 
 let Dp = props => {
 
-    let DivHeight = props.DivHeight;
-    let DivWidth = props.DivWidth;
+  let DivHeight = props.DivHeight;
+  let DivWidth = props.DivWidth;
 
-    let DivStyle = {
-        height: DivHeight,
-        width: DivWidth,
-        borderRadius: props.Circle ? '50%' : '0',
-        overflow: 'hidden'
-    }
+  let DivStyle = {
+    height: DivHeight,
+    width: DivWidth,
+    borderRadius: props.Circle ? '50%' : '0',
+    overflow: 'hidden'
+  }
 
-    let ImgStyle = {
-        objectFit: 'cover',
-        objectPosition: 'center',
-        width: '100%',
-        height: '100%'
-    }
+  let ImgStyle = {
+    objectFit: 'cover',
+    objectPosition: 'center',
+    width: '100%',
+    height: '100%'
+  }
 
-    return (
-        <div style={DivStyle}>
-            <img style={ImgStyle} src={Pic} alt='Profile Picture'/>
-        </div>
-    )
+  return (
+    <div style={DivStyle}>
+      <img style={ImgStyle} src={Pic} alt='display' />
+    </div>
+  )
 }
 
 export default Dp;
--- client/src/User/components/container/TopHeader/TopHeader.js
+++ client/src/User/components/container/TopHeader/TopHeader.js
@@ -4,49 +4,49 @@ import './TopHeader.css';
 import { Link } from 'react-router-dom';
 
 import Logo from '../../../../components/Brand/Brand';
-import Dp from '../../common/Dp';
+import Dp from '../../Common/Dp';
 
-import { FullName } from '../../common/Info';
+import { FullName } from '../../Common/Info';
 
-class TopHeader extends React.Component  {
+class TopHeader extends React.Component {
 
-	state={
-		UserDropdown: false
-	}
+  state = {
+    UserDropdown: false
+  }
 
-	showDropDown = () => {
-		this.setState(prevState => ({
-			UserDropdown: !prevState.UserDropdown
-		}))
-	}
+  showDropDown = () => {
+    this.setState( prevState => ( {
+      UserDropdown: !prevState.UserDropdown
+    } ) )
+  }
 
-	render() {
-		return (
-			<header className='TopHeader'>
-				<Logo color='var(--color1)'/>
-				<section onClick={this.showDropDown} className='UserSection' title='Click for options'>
-					<Dp Circle DivHeight='40px' DivWidth='40px' />
-					<i className='Caret fa fa-caret-down'></i>
-				</section>
-				{this.state.UserDropdown ?
-					<div className='UserDropdown'>
-						<ul>
-							<li className='fullname'>{FullName}</li>
-							<li>
-								<a href='#'><i className='fa fa-cog'></i>&nbsp;&nbsp;&nbsp;&nbsp;Settings</a>
-							</li>	
-							<li>
-								<a href='#'><i className='fa fa-close'></i>&nbsp;&nbsp;&nbsp;&nbsp;Log Out</a>
-							</li>
-						</ul>	
-					</div>
-					:
-					null
-				}
-				
-			</header>
-		)
-	}
+  render() {
+    return (
+      <header className='TopHeader'>
+        <Logo color='var(--color1)' />
+        <section onClick={this.showDropDown} className='UserSection' title='Click for options'>
+          <Dp Circle DivHeight='40px' DivWidth='40px' />
+          <i className='Caret fa fa-caret-down'></i>
+        </section>
+        {this.state.UserDropdown ?
+          <div className='UserDropdown'>
+            <ul>
+              <li className='fullname'>{FullName}</li>
+              <li>
+                <Link href='#'><i className='fa fa-cog'></i>&nbsp;&nbsp;&nbsp;&nbsp;Settings</Link>
+              </li>
+              <li>
+                <Link href='#'><i className='fa fa-close'></i>&nbsp;&nbsp;&nbsp;&nbsp;Log Out</Link>
+              </li>
+            </ul>
+          </div>
+          :
+          null
+        }
+
+      </header>
+    )
+  }
 };
 
 export default TopHeader;

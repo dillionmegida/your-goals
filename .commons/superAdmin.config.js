6822ec2778ac1cd957d42adf9e8cae9563d26f18
Favour George C <phavorsparks@gmail.com>
2019-10-01 09:58:55 +0100

    add config

---
+++ .commons/superAdmin.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  firstName: "admin",
+  lastName: "admin",
+  email: "admin@goals.com",
+  gender: "male",
+  password: "admin",
+  profileImage: "",
+  maritalStatus: "single",
+  phone: "0000",
+  referenceNumber: "admin@super",
+  type: "administrator",
+  permissions: ["canUpdateOtherAdmins", "canViewOtherAdmins", "canApprove", "canReject", "canComment", "canCreateAdmin", "canDelete", "canEdit", "canSuspend", "canCreate", "canUpdate"]
+}

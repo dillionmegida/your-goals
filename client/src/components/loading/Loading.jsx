14c49b7fa094cafc8f9f5df7e8c3451f5124a196
Favour George C <phavorsparks@gmail.com>
2019-10-01 10:45:16 +0100

    setup apollo client

---
+++ client/src/components/loading/Loading.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Grid, CircularProgress } from '@material-ui/core';
+
+export default () => (
+  <Grid container>
+    <CircularProgress />
+  </Grid>
+)
---
+++ client/src/config/apollo.config.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react'
+import { ApolloProvider } from '@apollo/react-hooks'
+import { persistCache } from 'apollo-cache-persist'
+import localForage from 'localforage'
+import ApolloClient, { InMemoryCache } from 'apollo-boost'
+import { defaults, resolvers } from './/apollo.gql'
+import Loading from '../components/loading/Loading'
+
+const SCHEMA_VERSION = '3'
+const SCHEMA_VERSION_KEY = 'apollo-schema-version'
+
+const mode = process.env.NODE_ENV
+const uri = mode === 'production' ?
+  'ourprodction server' :
+  'http://localhost:4000/graphql'
+
+class Persistor extends Component {
+  state = {
+    client: null,
+    loaded: false
+  }
+
+  async componentDidMount() {
+    const token = localStorage.getItem('goals_token') || ''
+    const cache = new InMemoryCache()
+    const client = new ApolloClient({
+      uri,
+      cache,
+      clientState: {
+        defaults,
+        resolvers
+      },
+      headers: {
+        authorization: `Bearer ${token}`
+      },
+      // connectToDevTools: true // mode ? mode : false
+    })
+
+    const data = {
+      token: token,
+      networkStatus: {
+        __typename: 'NetworkStatus',
+        isConnected: false,
+      }
+    }
+
+    cache.writeData({ data })
+
+    try {
+      const currentVersion = await localForage.getItem(SCHEMA_VERSION_KEY)
+      if (currentVersion !== SCHEMA_VERSION) {
+        await localForage.clear()
+        await localForage.setItem(SCHEMA_VERSION_KEY, SCHEMA_VERSION)
+      }
+
+      await persistCache({
+        cache,
+        debug: true,
+        storage: localForage
+      })
+    } catch (error) {
+      console.log(`Error restoring Apollo cache: ${error}`)
+    }
+
+    this.setState({
+      client,
+      loaded: true
+    })
+  }
+
+  render() {
+    const { client, loaded } = this.state;
+
+    if (!loaded) return <Loading />
+
+    return (
+      <ApolloProvider client={client}>
+        {this.props.children}
+      </ApolloProvider>
+    )
+  }
+}
+
+export default Persistor

4610eeea517d9444f1b4b4c58ead517d411c3cd2
Favour George C <phavorsparks@gmail.com>
2019-10-01 09:40:00 +0100

    update with config and starting up server

--- server/index.js
+++ server/index.js
@@ -1,43 +1,42 @@
-require( './src/config/config' )
+require('./src/config/config')
 const { MONGODB_URI_ONLINE, MONGODB_URI_OFFLINE, PORT, SSL_PORT } = process.env
-const { ApolloServer } = require( 'apollo-server' )
-const fs = require( 'fs' )
-const path = require( 'path' )
-const https = require( 'https' )
-const DB = require( './src/database' )
-// const superAdminDetails = require( './src/config/superAdmin.config' )
-// const superAdminDetails = require('./src/config/config.json');
+const { ApolloServer } = require('apollo-server')
+const fs = require('fs')
+const path = require('path')
+const https = require('https')
+const DB = require('./src/database')
+const superAdminDetails = require('./src/config/superAdmin.config.js')
 
-const typeDefs = require( './src/types' )
-const resolvers = require( './src/resolvers' )
-const dataSources = require( './src/datasources' )
+const typeDefs = require('./src/types')
+const resolvers = require('./src/resolvers')
+const dataSources = require('./src/datasources')
 
-const { getUser } = require( './src/utils/getUser' )
+const { getUser } = require('./src/utils/getUser')
 
-// new DB( superAdminDetails ).connect( MONGODB_URI_ONLINE )
+new DB(superAdminDetails).connect(MONGODB_URI_OFFLINE)
 
-const server = new ApolloServer( {
+const server = new ApolloServer({
   cors: true,
   typeDefs,
   resolvers,
-  context: async ( { req } ) => {
+  context: async ({ req }) => {
     const token = req.headers.authorization || '';
 
-    const AuthUser = await getUser( token );
+    const AuthUser = await getUser(token);
 
     return { AuthUser };
   },
-  dataSources: () => ( dataSources )
-} )
+  dataSources: () => (dataSources)
+})
 
-server.listen( PORT ).then( ( { url } ) => {
-  console.log( `🚀 Server ready at ${url}` )
-} )
+server.listen(PORT).then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`)
+})
 
-https.createServer( {
-  key: fs.readFileSync( path.join( process.cwd(), '/key.pem' ) ),
-  cert: fs.readFileSync( path.join( process.cwd(), '/cert.pem' ) )
-} )
-  .listen( SSL_PORT || 4141, () => {
-    console.log( `HTTPS server running on https://localhost:${SSL_PORT || 4141}/` )
-  } ).setTimeout( 780000 )
+https.createServer({
+  key: fs.readFileSync(path.join(process.cwd(), '/key.pem')),
+  cert: fs.readFileSync(path.join(process.cwd(), '/cert.pem'))
+})
+  .listen(SSL_PORT || 4141, () => {
+    console.log(`HTTPS server running on https://localhost:${SSL_PORT || 4141}/`)
+  }).setTimeout(780000)

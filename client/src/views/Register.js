da724302cf09f935bf2057434a7b89bdee6cd68c
Favour George C <phavorsparks@gmail.com>
2019-10-02 07:43:42 +0100

    add form state

--- client/src/views/Register.js
+++ client/src/views/Register.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Layout from '../components/Layout/Layout';
 import Quotes from '../components/Common/Quotes';
 
 import '../styles/LoginAndRegister.css';
 
+const initialState = {
+  name: '',
+  email: '',
+  username: '',
+  password: '',
+  confirmPassword: ''
+}
+
 let Register = () => {
+  const [state, setState] = useState(initialState)
+
+  const handleSubmit = e => {
+    e.preventDefault()
+
+
+  }
+
   return (
     <Layout>
       <section className='AuthSection'>
@@ -15,12 +31,14 @@ let Register = () => {
             <Link className='toAlternative' to='login'>
               Or Login
             </Link>
-            <input type='text' placeholder='Full Name' />
-            <input type='text' placeholder='Email Address' />
-            <input type='text' placeholder='Username' />
-            <input type='text' placeholder='*****' />
-            <input type='text' placeholder='*****' />
-            <input type='submit' className='RegisterBtn' value='Submit'/>
+            <form onSubmit={handleSubmit}>
+              <input type='text' name="name" value={state.name} placeholder='Full Name' />
+              <input type='text' name="email" value={state.email} placeholder='Email Address' />
+              <input type='text' name="username" value={state.username} placeholder='Username' />
+              <input type='password' name="password" value={state.password} placeholder='Password' />
+              <input type='password' name="confirmPassword" value={state.confirmPassword} placeholder='Confirm Password' />
+              <input type='submit' className='RegisterBtn' value='Submit' />
+            </form>
           </section>
         </section>
         <section className='Quotes'>
